366ba290dc3b758b3a4d79da671905761f87870f
jackgusler <guslerj1@newpaltz.edu>
2024-06-23 16:44:40 -0400

    format genre names consistently

--- client/src/components/GenreCard.vue
+++ client/src/components/GenreCard.vue
@@ -7,11 +7,27 @@ const props = defineProps<{
     listIndex: number;
     centerIndex: number;
 }>();
+
+function formatGenre(genre: string) {
+    const exceptions = ["mo", "uk", "dj"]; // Add more exceptions as needed
+    return genre
+        .split(' ')
+        .map(word => {
+            // Check if the word is an exception
+            if (exceptions.includes(word.toLowerCase())) {
+                return word.toUpperCase(); // Return the word in uppercase if it's an exception
+            } else {
+                // Capitalize the first letter of the word and make the rest lowercase
+                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            }
+        })
+        .join(' ');
+}
 </script>
 
 <template>
     <div class="card d-flex flex-column align-items-center" style="width: 14rem; position: relative;">
-        <h5 class="mt-2 mx-3 text-center truncate">{{ props.genre.genre }}</h5>
+        <h5 class="mt-2 mx-3 text-center truncate">{{ formatGenre(props.genre.genre) }}</h5>
         <a :href="props.genre.artist.external_urls.spotify" target="_blank" class="text-decoration-none text-dark">
             <template v-if="props.listIndex === props.centerIndex">
                 <img v-tooltip="{ content: 'Open Spotify', theme: 'tooltip-top' }"
